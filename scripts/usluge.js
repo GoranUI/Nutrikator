7179470792016eb574f1eb33aecc2effb8aea3ad
Goran Petkovic <88789363+GoranUI@users.noreply.github.com>
2023-03-22 14:05:49 +0100

    Add files via upload

--- scripts/usluge.js
+++ scripts/usluge.js
@@ -1,47 +1,73 @@
-$(document).ready(function() {
-    var count = 1;
+
+      $(document).ready(function() {
+        var count = 1;
+        var images = [
+          "assets/images/image1.png",
+          "assets/images/image2.png",
+          "assets/images/image3.png",
+          "assets/images/image4.png",
+          "assets/images/image5.png",
+          "assets/images/image6.png"
+        ];
+        var titles = [
+          "01",
+          "02",
+          "03",
+          "04",
+          "05",
+          "06"
+        ];
+        var paragraphs = [
+          "Izračunavanje energetske vrednosti proizvoda i njegovih nutritivnih sastojaka prema standardima deklarisanja -",
+          "Nulla auctor enim eu est pharetra, id eleifend felis maximus. Aenean vitae urna ut lectus consequat commodo. ",
+          "Aliquam interdum ipsum purus, sed dapibus sapien mollis vel. Suspendisse porttitor posuere purus, in maximus nunc consectetur eget.",
+          "Proin bibendum risus et velit pharetra porttitor. ",
+          "Fusce placerat quis velit vel pretium. Curabitur viverra molestie felis, ut varius sapien venenatis eget. In ullamcorper ante eu ultrices euismod. Suspendisse fermentum convallis nulla, sed."
+        ];
+
+        var paragraphs2 = [
+          "proteini, ukupne masti, zasićene masti, ugljeni hidrati, šećeri</span>",          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at mi vel elit vehicula pulvinar. Vestibulum lacinia magna et ante hendrerit, nec vestibulum nibh commodo. Etiam eleifend justo at interdum semper.",
+          "Mauris rutrum lorem id dolor sollicitudin, ac commodo ex convallis.",
+          "Proin pellentesque urna eget orci gravida, eu cursus velit laoreet. Aliquam quis quam nisi. Suspendisse eu aliquet nibh.",
+          "Nam eget eleifend elit, nec lacinia nisl. Donec pulvinar, enim quis malesuada maximus, velit velit vehicula justo, non aliquet mi magna non est.",
+          "Integer pharetra vulputate mauris vel hendrerit."
+        ];
+   
+  $(".left-button").prop("disabled", true); // disable left button initially
+  
+  $(".left-button").click(function() {
+    if (count > 1) {
+      count--;
+      updateContent();
+    }
+  });
+  
+  $(".right-button").click(function() {
+    if (count < 6) {
+      count++;
+      updateContent();
+    }
+  });
+  
+  function updateContent() {
+    // update image, title, and counter
+    $(".section-one img-right").attr("src", images[count-1]);
+    $(".section-one .text-container h2").text(titles[count-1]);
+    $(".section-one .text-container .p-1st").text(paragraphs[count-1]);
+    $(".section-one .text-container .p-2nd").text(paragraphs2[count-1]);
+    $(".section-one .button-container .counter").text(count + " of 6");
     
-    var images = [  "assets/images/image1.png",
-                    "assets/images/image2.png",
-                    "assets/images/image3.png",
-                    "assets/images/image4.png",
-                    "assets/images/image5.png",
-                    "assets/images/image6.png"];
+    // enable/disable buttons based on count
+    if (count === 1) {
+      $(".left-button").prop("disabled", true).addClass("inactive");
+    } else {
+      $(".left-button").prop("disabled", false).removeClass("inactive");
+    }
     
-    var titles = ["01", "02", "03", "04", "05", "06"];
-    
-    var paragraphs = [
-      "Izračunavanje energetske vrednosti proizvoda i njegovih nutritivnih sastojaka prema standardima deklarisanja - proteini, ukupne masti, zasićene masti, ugljeni hidrati, šećeri",
-      "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
-      "Vestibulum vitae imperdiet nisl. In scelerisque, dolor sit amet blandit rutrum, velit enim interdum nibh, vel auctor nibh nibh eu tortor.",
-      "Donec aliquam ultricies risus, eu consequat turpis feugiat id. Morbi fringilla justo eu leo tristique, ac malesuada dolor consectetur.",
-      "Sed porttitor augue a elit posuere, eu efficitur libero pellentesque. Fusce eu turpis vel nisi euismod hendrerit.",
-      "Nulla facilisi. Sed hendrerit sapien vel arcu tincidunt bibendum. Nam porttitor arcu in elit volutpat, eu pulvinar libero commodo."
-    ];
-    
-    $(".left-button").click(function() {
-        if (count > 1) {
-            count--;
-          } else {
-            count = 6;
-          }
-          
-          $(".image-container img").attr("src", images[count-1]);
-          $(".text-container h2").text(titles[count-1]);
-          $(".text-container p").text(paragraphs[count-1]);
-          $(".text-container .counter").text(count + " of 6");
-        });
-        
-        $(".right-button").click(function() {
-          if (count < 6) {
-            count++;
-          } else {
-            count = 1;
-          }
-          
-          $(".image-container img").attr("src", images[count-1]);
-          $(".text-container h2").text(titles[count-1]);
-          $(".text-container p").text(paragraphs[count-1]);
-          $(".text-container .counter").text(count + " of 6");
-        });
-      });  
+    if (count === 6) {
+      $(".right-button").prop("disabled", true).addClass("inactive");
+    } else {
+      $(".right-button").prop("disabled", false).removeClass("inactive");
+    }
+  }
+});s
