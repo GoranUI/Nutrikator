6e02fc347a4b4051490443c31d6223af79187f5e
Goran Petkovic <88789363+GoranUI@users.noreply.github.com>
2023-03-24 04:35:44 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,12 @@
-function parallaxIt(target, movement) {
-  TweenMax.to(target, 0.5, {
-    x: (mouse.x - rect.width / 2) / rect.width * movement,
-    y: (mouse.y - rect.height / 2) / rect.height * movement,
-  });
+function parallaxIt(target, movement, reset) {
+  if (reset) {
+    TweenMax.to(target, 0.5, { x: 0, y: 0 });
+  } else {
+    TweenMax.to(target, 0.5, {
+      x: (mouse.x - rect.width / 2) / rect.width * movement,
+      y: (mouse.y - rect.height / 2) / rect.height * movement,
+    });
+  }
 }
  
 let rect = {};
@@ -14,18 +18,21 @@ $(window).on('resize scroll', () => {
  
 $(() => {
   rect = $('#image-container')[0].getBoundingClientRect();
- 
+
   $('#image-container').mousemove(e => {
     mouse.moved = true;
     mouse.x = e.clientX - rect.left;
     mouse.y = e.clientY - rect.top;
   });
-   
+
+  $('#image-container').mouseleave(() => {
+    parallaxIt(".header-product-images", -50, true);
+  });
+
   // Ticker event will be called on every frame
   TweenLite.ticker.addEventListener('tick', () => {
-    if (mouse.moved) {    
-      parallaxIt(".header-product-images", -100);
-      parallaxIt(".header-image-wrapper", -30);
+    if (mouse.moved) {
+      parallaxIt(".header-product-images", -50, false);
     }
     mouse.moved = false;
   });
