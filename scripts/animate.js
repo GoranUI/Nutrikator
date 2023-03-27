d6d5b3f41f207e6dca2320bc88192580ae601b35
Goran Petkovic <88789363+GoranUI@users.noreply.github.com>
2023-03-27 15:53:51 +0200

    Add files via upload

--- scripts/animate.js
+++ scripts/animate.js
@@ -15,23 +15,3 @@ $(document).ready(function() {
 
 
   
-  // Set the amount of pixels to scroll per mouse wheel tick
-const scrollAmount = 100;
-
-// Add a wheel event listener to the document
-$(document).on('wheel', function(event) {
-  // Get the direction of the scroll (-1 for up, 1 for down)
-  const direction = Math.sign(event.originalEvent.deltaY);
-
-  // Get the section to scroll to
-  const currentSection = $(':target').length ? $(':target') : $('section:first-child');
-  const nextSection = direction === 1 ? currentSection.next('section') : currentSection.prev('section');
-
-  // Scroll to the next section
-  if (nextSection.length) {
-    event.preventDefault();
-    $('html, body').animate({
-      scrollTop: nextSection.offset().top
-    }, 'smooth');
-  }
-});
