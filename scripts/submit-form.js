815d9583e0100365f0ff93d638f1279a55c158a1
Goran Petkovic <88789363+GoranUI@users.noreply.github.com>
2023-03-30 16:14:00 +0200

    Update submit-form.js

--- scripts/submit-form.js
+++ scripts/submit-form.js
@@ -2,13 +2,35 @@ $(document).ready(function() {
   // Add event listener to form submit button
   $(".emailbutton").click(function(event) {
     event.preventDefault(); // Prevents the default form submission behavior
-    showSuccess(); // Calls the showSuccess function
+    submitForm(); // Calls the submitForm function to submit the form
   });
 
-  // Define the showSuccess function to display the success div and submit the form
+  // Define the submitForm function to submit the form and show the success message
+  function submitForm() {
+    $.ajax({
+      url: "https://nutrikator.us8.list-manage.com/subscribe/post-json?u=df3336ed42ff630c06e4e65da&amp;id=8f4f41ec6a&amp;f_id=00c800e0f0&c=?",
+      type: "GET",
+      dataType: "json",
+      data: $("#mc-embedded-subscribe-form").serialize(),
+      success: function(response) {
+        // Check if the form was submitted successfully
+        if (response.result === "success") {
+          showSuccess(); // Call the showSuccess function to display the success message
+        } else {
+          // Handle form submission errors
+          console.log("Form submission failed: " + response.msg);
+        }
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        // Handle AJAX errors
+        console.log("AJAX error: " + textStatus + " " + errorThrown);
+      }
+    });
+  }
+
+  // Define the showSuccess function to display the success message
   function showSuccess() {
     $("#success-message").show(); // Shows the success div
-    $("#mc-embedded-subscribe-form").submit(); // Submits the form
   }
 });
 
